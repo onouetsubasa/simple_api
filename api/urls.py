80bf8c2390ef42c1b46ebd5969da2346cfdcc6b9
onouetsubasa <onoue.wing0412@gmail.com>
2018-06-16 01:10:54 +0900

    just created

---
+++ api/urls.py
@@ -0,0 +1,6 @@
+from rest_framework import routers
+from .views import UserViewSet, EntryViewSet
+
+router = routers.DefaultRouter()
+router.register('users', UserViewSet)
+router.register('entries', EntryViewSet)
--- config/urls.py
+++ config/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url, include
 from django.contrib import admin
-from django.urls import path
+
+from api.urls import router as api_router
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
+    url('api/', include(api_router.urls)),
 ]
